63e20daeb00d98be017e92c1a170d7aafcadc948
Claudio Satriano <satriano@gmail.com>
2024-02-10 18:54:59 +0100

    Add docstring to __init__.py

--- requake/__init__.py
+++ requake/__init__.py
@@ -1,3 +1,13 @@
+# -*- coding: utf8 -*-
+"""
+Initialize requake package.
+
+:copyright:
+    2021-2024 Claudio Satriano <satriano@ipgp.fr>
+:license:
+    CeCILL Free Software License Agreement, Version 2.1
+    (http://www.cecill.info/index.en.html)
+"""
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
