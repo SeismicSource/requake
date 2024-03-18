0ba64f031dad738afc67ed311c093798ee890554
Claudio Satriano <satriano@gmail.com>
2024-03-18 15:21:21 +0100

    Initial support for Sphinx docs

---
+++ docs/conf.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""Sphinx configuration file."""
+# pylint: disable=wrong-import-position,invalid-name
+import sys
+import os
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.join(os.path.abspath('..'), 'requake'))
+from requake._version import get_versions  # NOQA
+__version__ = get_versions()['version']
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Requake'
+copyright = '2021-2024, Claudio Satriano'  # pylint: disable=redefined-builtin
+author = 'Claudio Satriano'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.doctest',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.intersphinx',
+    'sphinx_favicon',
+    'sphinx_rtd_theme',
+    # 'sphinxcontrib.bibtex',
+    'sphinx_mdinclude',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+html_logo = '../imgs/Requake_logo_white.svg'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. Requake documentation master file, created by
+   sphinx-quickstart on Mon Mar 18 14:50:23 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Requake's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
