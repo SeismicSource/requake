c4278affc8617b253ec0c42a36386b54a2b74401
Claudio Satriano <satriano@gmail.com>
2024-04-04 11:44:59 +0200

    Nice error message in case of unhandled exception

--- requake/main.py
+++ requake/main.py
@@ -14,8 +14,8 @@ Main script for Requake.
 # pylint: disable=relative-beyond-top-level,import-outside-toplevel
 
 
-def main():
-    """Main function for Requake."""
+def run():
+    """Run Requake."""
     from .config.parse_arguments import parse_arguments
     args = parse_arguments()
     from .config.rq_setup import configure, rq_exit
@@ -57,3 +57,44 @@ def main():
         from .families import build_templates
         build_templates(config)
     rq_exit(0)
+
+
+def main():
+    """Main entry point for Requake."""
+    try:
+        run()
+    # pylint: disable=broad-except
+    except Exception as e:
+        import sys
+        import traceback
+        from .config.rq_setup import rq_exit
+        from . import __version__
+        sys.stderr.write("""
+# BEGIN TRACEBACK #############################################################
+""")
+        sys.stderr.write('\n')
+        traceback.print_exc()
+        sys.stderr.write("""
+# END TRACEBACK ###############################################################
+""")
+        sys.stderr.write("""
+
+Congratulations, you've found a bug in Requake! 🐞
+
+Please report it on https://github.com/SeismicSource/requake/issues
+or by email to satriano@ipgp.fr.
+
+Include the following information in your report:
+
+""")
+        sys.stderr.write(f'  Requake version: {__version__}\n')
+        sys.stderr.write(f'  Python version: {sys.version}\n')
+        sys.stderr.write(f'  Platform: {sys.platform}\n')
+        sys.stderr.write(f'  Command line: {" ".join(sys.argv)}\n')
+        sys.stderr.write(f'  Error message: {str(e)}\n')
+        sys.stderr.write('\n')
+        sys.stderr.write(
+            'Also, please copy and paste the traceback above in your '
+            'report.\n\n')
+        sys.stderr.write('Thank you for your help!\n\n')
+        rq_exit(1)
