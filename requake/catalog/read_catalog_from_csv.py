9662b85aac2274fde95949b5e443d8379a4549bd
Claudio Satriano <satriano@gmail.com>
2024-06-13 11:54:56 +0200

    read_catalog_from_csv: skip rows with invalid orig time and log error

--- requake/catalog/read_catalog_from_csv.py
+++ requake/catalog/read_catalog_from_csv.py
@@ -183,17 +183,23 @@ def read_catalog_from_csv(filename):
                     year=year, month=month, day=day,
                     hour=hour, minute=minute, second=0) + seconds
             else:
+                orig_time_str = row[fields['orig_time']]
                 try:
-                    orig_time = UTCDateTime(row[fields['orig_time']])
+                    orig_time = UTCDateTime(orig_time_str)
                 except ValueError:
                     # one last try: check if the time is in the format
                     # YYYYMMDD.hhmmss.
                     # Replace the dot with a space, pad with zeros
                     # and try again
-                    orig_time = UTCDateTime(
-                        row[fields['orig_time']]
-                        .replace('.', ' ').ljust(15, '0')
-                    )
+                    try:
+                        orig_time = UTCDateTime(
+                            orig_time_str.replace('.', ' ').ljust(15, '0')
+                        )
+                    except ValueError:
+                        logger.error(
+                            f'Unable to parse origin time at row {n+2}: '
+                            f'"{orig_time_str}"')
+                        continue
             row[None] = None
             ev = RequakeEvent()
             ev.orig_time = orig_time
